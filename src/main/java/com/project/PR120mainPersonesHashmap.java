16337458c57106e81776121314eadeb92041a675
adriamartinez294 <adriamartinez870@gmail.com>
2024-10-03 18:09:14 +0200

    PR120 fet
    fet

--- src/main/java/com/project/PR120mainPersonesHashmap.java
+++ src/main/java/com/project/PR120mainPersonesHashmap.java
@@ -9,7 +9,7 @@ import com.project.excepcions.IOFitxerExcepcio;
 public class PR120mainPersonesHashmap {
     private static String filePath = System.getProperty("user.dir") + "/data/PR120persones.dat";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         HashMap<String, Integer> persones = new HashMap<>();
         persones.put("Anna", 25);
         persones.put("Bernat", 30);
@@ -36,12 +36,45 @@ public class PR120mainPersonesHashmap {
     }
 
     // Mètode per escriure les persones al fitxer
-    public static void escriurePersones(HashMap<String, Integer> persones) throws IOFitxerExcepcio {
+    public static void escriurePersones(HashMap<String, Integer> persones) throws IOFitxerExcepcio, FileNotFoundException, IOException {
        // *************** CODI PRÀCTICA **********************/
+
+            try (FileOutputStream fos = new FileOutputStream(getFilePath());
+             DataOutputStream dos = new DataOutputStream(fos)) {
+
+                for (HashMap.Entry<String, Integer> entry : persones.entrySet()) {
+                    String nom = entry.getKey();
+                    Integer edad = entry.getValue();
+
+                    dos.writeUTF(nom);
+                    dos.writeInt(edad);
+
+                    dos.flush();
+   
+        }
+       }
     }
 
     // Mètode per llegir les persones des del fitxer
-    public static void llegirPersones() throws IOFitxerExcepcio {
+    public static void llegirPersones() throws IOFitxerExcepcio, FileNotFoundException, IOException {
         // *************** CODI PRÀCTICA **********************/
+        HashMap<String, Integer> persones = new HashMap<>();
+
+        try (FileInputStream fis = new FileInputStream(getFilePath());
+             DataInputStream dis = new DataInputStream(fis)) {
+                while (true){
+                    try {
+                        String key = dis.readUTF();
+                        int value = dis.readInt();
+                        persones.put(key, value);
+    
+                    } catch (EOFException eof) {
+                        break;
+                    }
+                }
+                for (HashMap.Entry<String, Integer> entry : persones.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue() + " anys");
+                }
+            }
     }
 }
